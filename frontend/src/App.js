7d6292387c8cd7674634cb9649cb6d02cccc758e
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2021-06-17 19:30:23 -0300

    feat: :sparkles: Add some assets to project
    Images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
     
-    <h1>Hello Omnistack</h1>
+    <img src = {logo} alt = "Tindev "></img>
 
   );
 }
