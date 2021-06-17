50a91e79d5d759d81888e81ff913150084da0e6d
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2021-06-16 23:42:22 -0300

    feat: :sparkles: Add controller DislikeController and configure routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,11 +4,15 @@ const DevController = require('./controllers/DevController');
 
 const LikeController = require('./controllers/LikeController');
 
+const DislikeController = require('./controllers/DislikeController');
+
 const routes = express.Router();
 
 
 routes.post('/devs', DevController.store);
 
 routes.post('/devs/:devId/likes', LikeController.store);
+
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
