ae8592819a12b8d299d01d04ee5dbfaf14325a05
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2021-06-17 20:37:31 -0300

    feat: :sparkles: Add login component and style

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+
+export default function Login (){
+  return (
+      
+    <div className="login-container">
+      <form >
+        <img src = {logo} alt = "Tinderv" />
+        <input placeholder = "Digite seu usuário no Github"/>
+        <button type = "submit">Enviar</button>
+      </form>
+    </div>
+    
+
+  );
+}
