31c4263baf5e10f0bc9a512c56cc544205da5529
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2021-06-18 11:09:43 -0300

    Added: Return to Home from Image

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom';
 import './Main.css';
 
 import api from '../services/api'
@@ -45,7 +46,9 @@ export default function Main ({ match }) {
 
   return (
     <div className="main-container">
-      <img src = {logo} alt="Timdev" />
+      <Link to ="/">
+        <img src = {logo} alt="Timdev" />
+      </Link>
       { users.length > 0 ? (
         <ul>
           {users.map(user => (
