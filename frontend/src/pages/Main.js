9921131a5ad6d44433bd573d0051d1348751ee9c
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2021-06-18 00:49:38 -0300

    style: :lipstick: Add CSS stylization
    add CSS and some exemples in Main

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,97 @@
 import React from 'react';
+import './Main.css';
 
-export default function Main () {
-    return <h1>Hello World</h1>
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main ({ match }) {
+  return (
+    <div className="main-container">
+      <img src = {logo} alt="Timdev" />
+      <ul>
+
+        <li>
+          <img src="https://avatars.githubusercontent.com/u/2177042?v=4" alt="" />
+          <footer>
+
+            <strong>Raul FLP</strong>
+            <p>Estudante de Sistemas de Informação. UFOP</p>
+
+          </footer>
+          <div className="buttons">
+
+            <button className="button">
+              <img src={dislike} alt="Dislike" />
+            </button> 
+
+            <button className="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars.githubusercontent.com/u/2177042?v=4" alt="" />
+          <footer>
+
+            <strong>Raul FLP</strong>
+            <p>Estudante de Sistemas de Informação. UFOP</p>
+
+          </footer>
+          <div className="buttons">
+
+            <button className="button">
+              <img src={dislike} alt="Dislike" />
+            </button> 
+
+            <button className="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars.githubusercontent.com/u/2177042?v=4" alt="" />
+          <footer>
+
+            <strong>Raul FLP</strong>
+            <p>Estudante de Sistemas de Informação. UFOP</p>
+
+          </footer>
+          <div className="buttons">
+
+            <button className="button">
+              <img src={dislike} alt="Dislike" />
+            </button> 
+
+            <button className="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars.githubusercontent.com/u/2177042?v=4" alt="" />
+          <footer>
+
+            <strong>Raul FLP</strong>
+            <p>Estudante de Sistemas de Informação. UFOP</p>
+
+          </footer>
+          <div className="buttons">
+
+            <button className="button">
+              <img src={dislike} alt="Dislike" />
+            </button> 
+
+            <button className="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+  );
 }
