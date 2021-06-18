36a2df15dae59f7b4c2a7a05215391b3364fd395
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2021-06-17 22:04:56 -0300

    feat: :sparkles: Add routes and Main page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
 
-export default function Login (){
+export default function Login ({ history }){
+  const [ username, setUsername ] = useState('');
+
+  function handleSubmit (event){
+    event.preventDefault();
+
+    console.log(username);
+
+    history.push('/main');
+  }
+
   return (
       
     <div className="login-container">
-      <form >
+      <form onSubmit = {handleSubmit}>
         <img src = {logo} alt = "Tinderv" />
-        <input placeholder = "Digite seu usuário no Github"/>
+        <input 
+          placeholder = "Digite seu usuário no Github"
+          value = {username}
+          onChange = {event => setUsername(event.target.value)}
+        />
         <button type = "submit">Enviar</button>
       </form>
     </div>
